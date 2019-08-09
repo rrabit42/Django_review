1a9bcff92edbd9f9f1775b26fb643d8133a50515
rrabit42 <gegiraffe@naver.com>
2019-08-10 01:46:04 +0900

    djagno admin model custom

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+#기본 모델 admin으로 동작
+#admin.site.register(Item)
+
+#admin 모델 커스텀 가능
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'short_desc', 'price', 'is_publish']
+    list_display_links = ['name', 'short_desc']
+    list_filter = ['is_publish', 'updated_at']
+    # search는 OR query로 들어감
+    search_fields = ['name', 'desc']
+
+    def short_desc(self, item):
+        return item.desc[:20]
+
--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,12 @@ class Item(models.Model):
   name = models.CharField(max_length=100)
   desc = models.TextField(blank=True)
   price = models.PositiveIntegerField()
+  is_publish = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    # return '<{}> {}'.format(self.pk, self.name)
+    return f'<{self.pk}> {self.name}'
 
 
