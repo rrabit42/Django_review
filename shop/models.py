f94999fc681eff012beafcb2e3b9cb937860cb60
rrabit42 <gegiraffe@naver.com>
2019-08-18 20:29:24 +0900

    URL Reverse

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
+
 from askcompany.utils import uuid_upload_to
 
 
@@ -16,6 +18,13 @@ class Item(models.Model):
     # return '<{}> {}'.format(self.pk, self.name)
     return f'<{self.pk}> {self.name}'
 
+  def get_absolute_url(self):
+    # args: 튜플, kwargs: 딕셔너리
+    # return reverse('shop:item_detail', args=[self.pk])
+    # 얘랑 같은 의미
+    # shop은 namespace, item_detail은 url에서 name지정해 준 것
+    return reverse('shop:item_detail', kwargs={'pk': self.pk})
+    # 여기서 'pk'는 urls.py에 있는 <int:pk>의 pk와 맞춰주는 것!
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- shop/urls.py
+++ shop/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 register_converter(FourDigitYearConverter, 'yyyy')
 
+# url reverse에서 namespace
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.item_list),
-    path('<int:pk>/', views.item_detail)
+    path('', views.item_list, name='item_list'),
+    path('<int:pk>/', views.item_detail, name='item_detail')
     # re_path(r'^(?P<pk>\d+)/$', views.item_detail) : 옛날 url 방식
 ]
